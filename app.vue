b430ac22f8831a2d488c3fc4f9296de9504987f3
CMER <sz@cmer.eu.org>
2024-11-07 15:10:17 +0800

    "fix: “

--- app.vue
+++ app.vue
@@ -32,7 +32,6 @@ onMounted(() => {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&display=swap");
-
 @import url("https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&family=Noto+Sans+HK:wght@100..900&display=swap");
 
 * {
